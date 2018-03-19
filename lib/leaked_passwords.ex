40e50f6078deb51e4fbb0eb2d669e8d813b32590
Maarten Jacobs <m.jacobs@defacto.nl>
2018-03-19 13:36:34 +0100

    project initialization

---
+++ lib/leaked_passwords.ex
@@ -0,0 +1,18 @@
+defmodule LeakedPasswords do
+  @moduledoc """
+  Documentation for LeakedPasswords.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LeakedPasswords.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/leaked_passwords_test.exs
@@ -0,0 +1,8 @@
+defmodule LeakedPasswordsTest do
+  use ExUnit.Case
+  doctest LeakedPasswords
+
+  test "greets the world" do
+    assert LeakedPasswords.hello() == :world
+  end
+end
