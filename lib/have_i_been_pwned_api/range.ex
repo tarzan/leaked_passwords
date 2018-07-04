9a4b08bb0f1302ac11c5229a3ffdbc87fb7365c5
Maarten Jacobs <m.jacobs@defacto.nl>
2018-07-04 15:52:12 +0200

    define the default API adapter

--- lib/have_i_been_pwned_api/range.ex
+++ lib/have_i_been_pwned_api/range.ex
@@ -1,11 +1,13 @@
 defmodule HaveIBeenPwnedApi.Range do
-  @adapter Application.get_env(:leaked_passwords, :adapter)
+  @default_adapter HaveIBeenPwnedApi.Adapter
 
   def get(<<hash_prefix::bytes-size(5)>>) do
-    @adapter.request(:get, "/range/" <> String.upcase(hash_prefix))
+    adapter.request(:get, "/range/" <> String.upcase(hash_prefix))
   end
 
   def get!(<<hash_prefix::bytes-size(5)>>) do
-    @adapter.request!(:get, "/range/" <> String.upcase(hash_prefix))
+    adapter.request!(:get, "/range/" <> String.upcase(hash_prefix))
   end
+
+  defp adapter, do: Application.get_env(:leaked_passwords, :adapter) || @default_adapter
 end
