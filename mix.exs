199b83ba8160abcb0e46a3277757ec930cab5f38
Maarten Jacobs <m.jacobs@defacto.nl>
2020-06-25 21:37:23 +0200

    release v1 to hex.pm

--- mix.exs
+++ mix.exs
@@ -4,8 +4,15 @@ defmodule LeakedPasswords.MixProject do
   def project do
     [
       app: :leaked_passwords,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.8.0",
+      description: """
+      Wrapper around Troy Hunt's endpoints for checking whether a given password
+      has been leaked in any of his HaveIBeenPwned datasets.
+      This wrapper uses the 'safe' endpoints by first calculating the SHA1 and
+      then only POSTing the first 5 characters to the API endpoints.
+      """,
+      package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -22,10 +29,20 @@ defmodule LeakedPasswords.MixProject do
     ]
   end
 
+  defp package do
+    [
+      name: :leaked_passwords,
+      maintainers: ["Maarten Jacobs - @tarzan"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tarzan/leaked_passwords"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 1.0", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
       {:jason, "~> 1.2"},
       {:httpoison, "~> 1.7"}
     ]
